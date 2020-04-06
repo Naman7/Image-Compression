8d7b840d5b50387cf69b2d9db7b9d7b5282a63c5
Naman7 <namanjain129@gmail.com>
2020-04-06 23:44:15 +0530

    Update ImageCompression.py

--- ImageCompression.py
+++ ImageCompression.py
@@ -2,17 +2,18 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread("Cr7.jpg",0)
+img = cv2.imread("C:/Users/Naman/Desktop/cr7.jpg",0)
 
 a = np.asarray(img)
 
-q = int(input("Quality Level in % \n"))
+q = float(input("Quality Level in % \n"))
+
 u,s,v = np.linalg.svd(a, full_matrices=0)
 
 q = round((q * s.shape[0])/100)
 
-
 So = u.shape[0]*u.shape[1] + v.shape[0]*v.shape[1] + s.shape[0]**2
+org = ((s.shape[0])**2) * (u.shape[0] + v.shape[0])
 
 u=u[:, 0:q]
 v=v[0:q]
@@ -24,9 +25,7 @@ for i in range(q):
 z = np.matmul(u, x)
 z = np.matmul(z, v)
 
-org = (s.shape[0])**2 * (u.shape[0] + v.shape[1])
-now = q**2 * (a.shape[0] + q)
-
+now = q**2 * (u.shape[0] + q)
 Sn = u.shape[0]*q + q*v.shape[1] + q**2
 
 print("Reduction in computation is ", round(((org-now)/org)*100), "%")
